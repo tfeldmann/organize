1a159cfa2d7c97d237a9f70cc546a33055dd9de6
Thomas Feldmann <info@tfeldmann.de>
2017-09-22 16:31:39 +0200

    add files needed for pypi

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, 'organize', '__version__.py')) as f:
+    exec(f.read(), about)
+
+
+setup(
+    name='organize',
+    version=about['__version__'],
+    description='The file management automation tool.',
+    long_description=long_description,
+    author='Thomas Feldmann',
+    author_email='mail@tfeldmann.de',
+    url='https://github.com/tfeldmann/organize',
+    packages=find_packages(exclude=['test']),
+    entry_points={
+        'console_scripts': ['organize=organize.cli:main'],
+    },
+    install_requires=['appdirs', 'docopt', 'slate3k', 'pyyaml'],
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
