6dfde2b1e8a9c9d4b838d71cd897ea2bfdc03ce8
Thomas Feldmann <info@tfeldmann.de>
2017-11-01 16:23:40 +0100

    add regex docs

--- organize/filters/regex.py
+++ organize/filters/regex.py
@@ -9,7 +9,38 @@ class Regex(Filter):
     Matches filenames with the given regular expression
 
     :param str expr:
-        The regular expression to be matched
+        The regular expression to be matched.
+
+    Any named groups in your regular expression will be returned like this:
+
+    :returns:
+        - regex.[your named group]
+
+    Examples:
+        - Match an invoice with a regular expression:
+
+          .. code-block:: yaml
+
+            rules:
+              - folders: '~/Desktop'
+                filters:
+                  - Regex: '^RG(\d{12})-sig\.pdf$'
+                actions:
+                  - Move: '~/Documents/Invoices/1und1/'
+
+        - Match and extract data from filenames with regex named groups:
+          This is just like the previous example but we rename the invoice using
+          the invoice number extracted via the regular expression and the named
+          group ``the_number``.
+
+          .. code-block:: yaml
+
+            rules:
+              - folders: '~/Desktop'
+                filters:
+                  - Regex: '^RG(?P<the_number>\d{12})-sig\.pdf$'
+                actions:
+                  - Move: '~/Documents/Invoices/1und1/{the_number}.pdf'
     """
 
     def __init__(self, expr):
