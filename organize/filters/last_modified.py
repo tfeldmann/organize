4dd619415beb1f516db9e0a37645be736bcb59ee
Thomas Feldmann <info@tfeldmann.de>
2018-02-14 16:00:12 +0100

    add LastModified docs

--- organize/filters/last_modified.py
+++ organize/filters/last_modified.py
@@ -4,6 +4,51 @@ from .filter import Filter
 
 class LastModified(Filter):
 
+    """
+    Matches files by last modified date
+
+    :param int days:
+        specify number of days
+
+    :param int hours:
+        specify number of hours
+
+    :param int minutes:
+        specify number of minutes
+
+    :param str select:
+        either 'within' or 'before'. 'within' matches all file last modified
+        within the given time, 'before' all files last modified before the given
+        time.
+
+    Examples:
+        - Show all files on your desktop modified within the last 10 days:
+
+          .. code-block:: yaml
+
+            rules:
+              - folders: '~/Desktop'
+                filters:
+                  - LastModified:
+                      - days: 10
+                actions:
+                  - Echo: 'Was modified within the last 10 days'
+
+        - Show all files on your desktop which were modified at least 5 hours
+          ago:
+
+          .. code-block:: yaml
+
+            rules:
+              - folders: '~/Desktop'
+                filters:
+                  - LastModified:
+                      - hours: 5
+                      - select: 'before'
+                actions:
+                  - Echo: 'Was modified more than 5 hours ago.'
+    """
+
     def __init__(self, days=0, hours=0, minutes=0, seconds=0, select='within'):
         _select = select.strip().lower()
         if _select not in ('within', 'before'):
