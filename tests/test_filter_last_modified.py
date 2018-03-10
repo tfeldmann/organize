8fb18ac380c80d44e41c12427354e9070e7847c3
Thomas Feldmann <mail@tfeldmann.de>
2018-03-10 18:45:20 +0100

    update test

--- tests/test_filter_last_modified.py
+++ tests/test_filter_last_modified.py
@@ -8,7 +8,7 @@ from organize.utils import Path
 
 def test_min():
     now = datetime.now()
-    last_modified = LastModified(days=10, hours=12, select_mode='min')
+    last_modified = LastModified(days=10, hours=12, mode='older')
     with patch.object(last_modified, '_last_modified') as mock_lm:
         mock_lm.return_value = now - timedelta(days=10, hours=0)
         assert not last_modified.matches(Path('~'))
@@ -18,7 +18,7 @@ def test_min():
 
 def test_max():
     now = datetime.now()
-    last_modified = LastModified(days=10, hours=12, select_mode='max')
+    last_modified = LastModified(days=10, hours=12, mode='newer')
     with patch.object(last_modified, '_last_modified') as mock_lm:
         mock_lm.return_value = now - timedelta(days=10, hours=0)
         assert last_modified.matches(Path('~'))
