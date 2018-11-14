d3eb2fe9d67e79f2b16b795aa923efcbfb3d9fb3
Thomas Feldmann <mail@tfeldmann.de>
2018-11-14 16:41:51 +0100

    add python 3.7 to trove classifier list

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Utilities',
     ],
     project_urls={
