79df7679f5c7f2c8718ebae7c52a564456547277
dominic <gitkraken@domi.ninja>
2019-05-01 19:31:03 +0200

    pep8

--- organize/filters/filesize.py
+++ organize/filters/filesize.py
@@ -37,20 +37,13 @@ class FileSize(Filter):
     @staticmethod
     def _parse_size_arg(size_string):
         size_string = size_string.lower().strip()
-        size_number = re.findall(r'^([0-9\.]+)', size_string)[0]
-        size_unit = re.findall(r'([a-z]+)$', size_string)
-        if len(size_unit)>0:
-            size_unit=size_unit[0]
+        size_number = re.findall(r"^([0-9\.]+)", size_string)[0]
+        size_unit = re.findall(r"([a-z]+)$", size_string)
+        if len(size_unit) > 0:
+            size_unit = size_unit[0]
         else:
-            size_unit='b'
-        unit_lookup = {
-            'b': 0,
-            'k': 3,
-            'm': 6,
-            'g': 9,
-            't': 12,
-            'p': 15,
-        }
+            size_unit = "b"
+        unit_lookup = {"b": 0, "k": 3, "m": 6, "g": 9, "t": 12, "p": 15}
         return float(size_number) * pow(10, unit_lookup[size_unit.strip().lower()[0]])
 
     def __init__(self, smaller=None, bigger=None):
@@ -59,20 +52,21 @@ class FileSize(Filter):
         if bigger is not None:
             bigger = self._parse_size_arg(bigger)
 
-        self.smaller=smaller
-        self.bigger=bigger
+        self.smaller = smaller
+        self.bigger = bigger
 
     def matches(self, path):
         file_size = self._get_file_size(path)
-        return (self.smaller is None or (file_size <= self.smaller)) and \
-               (self.bigger is None or (file_size >= self.bigger))
+        return (self.smaller is None or (file_size <= self.smaller)) and (
+            self.bigger is None or (file_size >= self.bigger)
+        )
 
     def parse(self, path):
         file_size = self._get_file_size(path)
-        return {'filesize': file_size}
+        return {"filesize": file_size}
 
     def _get_file_size(self, path):
         return os.path.getsize(path)
 
     def __str__(self):
-        return f'FileSize({self.bigger} <= filesize <= {self.smaller})'
+        return f"FileSize({self.bigger} <= filesize <= {self.smaller})"
