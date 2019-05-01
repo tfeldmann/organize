a97a5d558bfd89b739a7b33d42943179aacf4ba2
dominic <gitkraken@domi.ninja>
2019-05-01 19:44:50 +0200

    pep8 and tiny refactor

--- organize/filters/__init__.py
+++ organize/filters/__init__.py
@@ -2,4 +2,4 @@ from .extension import Extension
 from .filename import Filename
 from .last_modified import LastModified
 from .regex import Regex
-from .filesize import FileSize
+from .filesize import Filesize
