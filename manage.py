423894f593e3484e0a70095d70f4cfe72504f247
Thomas Feldmann <mail@tfeldmann.de>
2019-07-26 11:22:42 +0200

    add git and pypi upload

--- manage.py
+++ manage.py
@@ -5,11 +5,20 @@ from datetime import datetime
 from pathlib import Path
 
 
-LIB_NAME = "organize"
+SRC_FOLDER = "organize"
 CURRENT_FOLDER = Path(__file__).resolve().parent
 
 
 def new_version(args):
+    """ creates and publishes a new version
+    - read and validate version number
+    - update __version__.py
+    - update pyproject.toml
+    - Search for 'WIP' in changelog and replace with current version and date
+    - create git tag
+    - push to github
+    - publish on pypi
+    """
     assert CURRENT_FOLDER == Path.cwd().resolve()
     version = args.version
 
@@ -29,7 +38,7 @@ def new_version(args):
         return
 
     # update library version
-    versionfile = CURRENT_FOLDER / LIB_NAME / "__version__.py"
+    versionfile = CURRENT_FOLDER / SRC_FOLDER / "__version__.py"
     with open(versionfile, "w") as f:
         print(f"Updating {versionfile}")
         f.write(f'__version__ = "{version}"\n')
@@ -38,28 +47,43 @@ def new_version(args):
     print("Updating pyproject.toml")
     subprocess.run(["poetry", "version", version], check=True)
 
+    # read changelog
     with open(CURRENT_FOLDER / "CHANGELOG.md", "r") as f:
         changelog = f.read()
 
-        # check if WIP section is in changelog
-        if "## WIP" not in changelog:
-            print("No work in progress found in changelog")
-            return
+    # check if WIP section is in changelog
+    wip_regex = re.compile(r"## WIP\n(.*?)(?=\n##)", re.MULTILINE | re.DOTALL)
+    match = wip_regex.search(changelog)
+    if not match:
+        print("No '## WIP' section found in changelog")
+        return
 
-        # change WIP to version number and date
-        changelog = changelog.replace(
-            "## WIP",
-            f"## v{version} (" + datetime.now().strftime("%Y-%m-%d") + ")",
-            1,
-        )
+    # change WIP to version number and date
+    changes = match.group(1)
+    today = datetime.now().strftime("%Y-%m-%d")
+    changelog = wip_regex.sub(f"## v{version} ({today})\n{changes}", changelog, count=1)
+
+    # write changelog
     with open(CURRENT_FOLDER / "CHANGELOG.md", "w") as f:
         f.write(changelog)
 
     # create git tag ('vXXX')
-    # push to origin
-    # create github release with changelog
+    if input("Create tag? [Y/n]").lower() in ["", "y"]:
+        subprocess.run(["git", "tag", "-a", f"v{version}", "-m", f"v{version}"])
+
+    # push to github
+    if input("Push to github? [Y/n]").lower() in ["", "y"]:
+        print("Pushing to github")
+        subprocess.run(["git", "push", "--follow-tags"], check=True)
+
     # upload to pypi
-    # trigger readthedocs
+    if input("Publish on Pypi? [Y/n]").lower() in ["", "y"]:
+        subprocess.run(["rm", "-rf", "dist"], check=True)
+        subprocess.run(["poetry", "build"], check=True)
+        subprocess.run(["poetry", "publish"], check=True)
+
+    # TODO: create github release with changelog
+    # TODO: trigger readthedocs?
     print("done.")
 
 
