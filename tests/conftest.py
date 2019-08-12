c60b5b71b5bb68a629fecbe6ae7d21f69c65733f
Thomas Feldmann <mail@tfeldmann.de>
2019-08-12 11:28:32 +0200

    working integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,60 +1,27 @@
 import os
 
 import pytest
-import yaml
 from mock import patch
 
 from organize.utils import Path
 
 
-def pytest_collect_file(parent, path):
-    if path.ext == ".yaml" and path.basename.startswith("test"):
-        return YamlFile(path, parent)
-
-
-class YamlFile(pytest.File):
-    def collect(self):
-        tests = yaml.safe_load_all(self.fspath.open())
-        for test in tests:
-            yield YamlItem(parent=self, name=test["description"], spec=test)
-
-
-class YamlItem(pytest.Item):
-    def __init__(self, parent, name, spec):
-        super().__init__(name, parent)
-        self.name = name
-        self.spec = spec
-
-    def runtest(self):
-        if not "config" in self.spec:
-            raise YamlException("no config found")
-
-
-    def repr_failure(self, excinfo):
-        """ called when self.runtest() raises an exception. """
-        if isinstance(excinfo.value, YamlException):
-            return "\n".join(
-                [
-                    "usecase execution failed",
-                    "   spec failed: %r: %r" % excinfo.value.args[1:3],
-                    "   no further details known at this point.",
-                ]
-            )
-
-    def reportinfo(self):
-        return self.fspath, 0, "usecase: %s" % self.name
-
-
-def create_filesystem(tmp_path, files):
+def create_filesystem(tmp_path, files, config):
+    # create files
     for f in files:
-        p = tmp_path / Path(f)
+        p = tmp_path / "files" / Path(f)
         p.parent.mkdir(parents=True, exist_ok=True)
         p.touch()
+    # create config
+    with (tmp_path / "config.yaml").open("w") as f:
+        f.write(config)
+    # change working directory
     os.chdir(str(tmp_path))
 
 
-class YamlException(Exception):
-    """ custom exception for error reporting. """
+def assertdir(path, *files):
+    os.chdir(str(path / "files"))
+    assert set(files) == set(str(x) for x in Path(".").glob("**/*") if x.is_file())
 
 
 @pytest.fixture
@@ -109,5 +76,4 @@ def mock_parent():
 def mock_mkdir():
     with patch.object(Path, "mkdir") as mck:
         yield mck
-
 
--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,70 +1,54 @@
-import os
-
-import pytest
-from conftest import create_filesystem
-
-from organize.config import Config
-from organize.core import execute_rules
+from conftest import assertdir, create_filesystem
+from organize.cli import main
 from organize.utils import Path
-
-FIXTURE_DIR = Path(__file__).parent.absolute() / "01_basic"
-
-
-def filenames(files):
-    return set([Path(str(f)).name for f in files])
 
 
 def test_filename_move(tmp_path):
-    create_filesystem(tmp_path, ["sub/test.PY"])
-    config = Config.from_string(
-        """
-        rules:
-        - folders: 'sub'
-          filters:
-          - Extension
-          actions:
-          - rename: '{path.stem}{path.stem}.{extension.lower}'
-        """
+    create_filesystem(
+        tmp_path,
+        files=["test.PY"],
+        config="""
+            rules:
+            - folders: files
+              filters:
+              - Extension
+              actions:
+              - rename: '{path.stem}{path.stem}.{extension.lower}'
+        """,
     )
-    execute_rules(config.rules, simulate=False)
-    assert (tmp_path / "sub" / "testtest.py").exists()
-    assert not (tmp_path / "sub" / "test.PY").exists()
+    main(["run", "--config-file=%s" % (tmp_path / "config.yaml")])
+    assertdir(tmp_path, "testtest.py")
 
 
-@pytest.mark.datafiles(FIXTURE_DIR)
-def test_basic(datafiles):
-    os.chdir(str(datafiles))
-    config = Config.from_string(
-        """
-        rules:
-        - folders: '.'
-          filters:
-            - filename: test
-          actions:
-            - copy: 'newname.pdf'
-        """
+def test_basic(tmp_path):
+    create_filesystem(
+        tmp_path,
+        files=["asd.txt", "newname 2.pdf", "newname.pdf", "test.pdf"],
+        config="""
+            rules:
+            - folders: files
+              filters:
+                - filename: test
+              actions:
+                - copy: files/newname.pdf
+        """,
     )
-    execute_rules(config.rules, simulate=False)
-    assert filenames(datafiles.listdir()) == {
-        "newname.pdf",
-        "newname 2.pdf",
-        "newname 3.pdf",
-        "test.pdf",
-        "asd.txt",
-        "config.yaml",
-    }
-
-
-@pytest.mark.datafiles(FIXTURE_DIR)
-def test_globstr(datafiles):
-    os.chdir(str(datafiles))
-    config = Config.from_string(
-        """
-        rules:
-        - folders: './*.pdf'
-          actions:
-          - trash
-        """
+    main(["run", "--config-file=%s" % (tmp_path / "config.yaml")])
+    assertdir(
+        tmp_path, "newname.pdf", "newname 2.pdf", "newname 3.pdf", "test.pdf", "asd.txt"
     )
-    execute_rules(config.rules, simulate=False)
-    assert filenames(datafiles.listdir()) == {"asd.txt", "config.yaml"}
+
+
+def test_globstr(tmp_path):
+    create_filesystem(
+        tmp_path,
+        files=["asd.txt", "newname 2.pdf", "newname.pdf", "test.pdf"],
+        config="""
+            rules:
+              - folders: 'file[s]/*.pdf'
+                actions:
+                  - trash
+        """,
+    )
+    main(["run", "--config-file=%s" % (tmp_path / "config.yaml")])
+    assertdir(tmp_path, "asd.txt")
