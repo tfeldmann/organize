665d1c20114b3204013d4ee615850ce62973e531
Thomas Feldmann <mail@tfeldmann.de>
2019-08-25 17:30:40 +0200

    add expected values

--- organize/filters/exif.py
+++ organize/filters/exif.py
@@ -11,31 +11,39 @@ class Exif(Filter):
     Filter by EXIF data
     """
 
-    def __init__(self, **kwargs):
-        self.kwargs = kwargs
+    def __init__(self, *args, **kwargs):
+        self.args = args  # expected exif keys
+        self.kwargs = kwargs  # exif keys with expected values
 
-    def convert_tags(self, tags):
+    def category_dict(self, tags):
         result = collections.defaultdict(dict)
         for key, value in tags.items():
             if " " in key:
                 category, field = key.split(" ", maxsplit=1)
-                result[category][field] = value.printable
+                result[category][field] = value
             else:
-                result[key] = value.printable
+                result[key] = value
         return result
 
     def matches(self, path):
         with path.open("rb") as f:
             exiftags = exifread.process_file(f, details=False)
-            if not exiftags:
-                return False
+        if not exiftags:
+            return False
 
-            tags = {k.lower(): v for k, v in exiftags.items()}
-            if tags:
-                for key, _ in self.kwargs.items():
-                    if key.lower() not in tags:
-                        return False
-                return self.convert_tags(tags)
+        tags = {k.lower(): v.printable for k, v in exiftags.items()}
+
+        # no match if expected tag is not found
+        normkey = lambda k: k.replace(".", " ").lower()
+        for key in self.args:
+            if normkey(key) not in tags:
+                return False
+        # no match if tag has not expected value
+        for key, value in self.kwargs.items():
+            key = normkey(key)
+            if not (key in tags and tags[key].lower() == value.lower()):
+                return False
+        return self.category_dict(tags)
 
     def pipeline(self, args):
         tags = self.matches(args.path)
