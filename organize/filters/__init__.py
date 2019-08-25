1a0b68f70af852f7222a4b73dd6770af617aa75a
Thomas Feldmann <mail@tfeldmann.de>
2019-08-25 15:09:44 +0200

    add first working version

--- organize/filters/__init__.py
+++ organize/filters/__init__.py
@@ -1,4 +1,5 @@
 from .created import Created
+from .exif import Exif
 from .extension import Extension
 from .filename import Filename
 from .filesize import FileSize
---
+++ organize/filters/exif.py
@@ -0,0 +1,108 @@
+import collections
+
+import exifread
+
+from organize.utils import flatten
+
+from .filter import Filter
+
+
+# taken from https://github.com/psf/requests/blob/v1.2.3/requests/structures.py#L37
+class CaseInsensitiveDict(collections.MutableMapping):
+    """
+    A case-insensitive ``dict``-like object.
+    Implements all methods and operations of
+    ``collections.MutableMapping`` as well as dict's ``copy``. Also
+    provides ``lower_items``.
+    All keys are expected to be strings. The structure remembers the
+    case of the last key to be set, and ``iter(instance)``,
+    ``keys()``, ``items()``, ``iterkeys()``, and ``iteritems()``
+    will contain case-sensitive keys. However, querying and contains
+    testing is case insensitive:
+        cid = CaseInsensitiveDict()
+        cid['Accept'] = 'application/json'
+        cid['aCCEPT'] == 'application/json'  # True
+        list(cid) == ['Accept']  # True
+    For example, ``headers['content-encoding']`` will return the
+    value of a ``'Content-Encoding'`` response header, regardless
+    of how the header name was originally stored.
+    If the constructor, ``.update``, or equality comparison
+    operations are given keys that have equal ``.lower()``s, the
+    behavior is undefined.
+    """
+
+    def __init__(self, data=None, **kwargs):
+        self._store = dict()
+        if data is None:
+            data = {}
+        self.update(data, **kwargs)
+
+    def __setitem__(self, key, value):
+        # Use the lowercased key for lookups, but store the actual
+        # key alongside the value.
+        self._store[key.lower()] = (key, value)
+
+    def __getitem__(self, key):
+        return self._store[key.lower()][1]
+
+    def __delitem__(self, key):
+        del self._store[key.lower()]
+
+    def __iter__(self):
+        return (casedkey for casedkey, mappedvalue in self._store.values())
+
+    def __len__(self):
+        return len(self._store)
+
+    def lower_items(self):
+        """Like iteritems(), but with all lowercase keys."""
+        return ((lowerkey, keyval[1]) for (lowerkey, keyval) in self._store.items())
+
+    def __eq__(self, other):
+        if isinstance(other, collections.Mapping):
+            other = CaseInsensitiveDict(other)
+        else:
+            return NotImplemented
+        # Compare insensitively
+        return dict(self.lower_items()) == dict(other.lower_items())
+
+    # Copy is required
+    def copy(self):
+        return CaseInsensitiveDict(self._store.values())
+
+    def __repr__(self):
+        return "%s(%r)" % (self.__class__.__name__, dict(self.items()))
+
+
+class Exif(Filter):
+
+    """
+    Filter by EXIF data
+    """
+
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def convert_tags(self, tags):
+        result = collections.defaultdict(dict)
+        for key, value in tags.items():
+            if " " in key:
+                category, field = key.split(" ", maxsplit=1)
+                result[category][field] = value.printable
+            else:
+                result[key] = value.printable
+        return result
+
+    def matches(self, path):
+        with path.open("rb") as f:
+            tags = exifread.process_file(f, details=False)
+            if tags:
+                return self.convert_tags(tags)
+
+    def pipeline(self, args):
+        tags = self.matches(args.path)
+        if tags:
+            return {"exif": tags}
+
+    def __str__(self):
+        return "EXIF(%s)" % ", ".join(self.kwargs.items())
