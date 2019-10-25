3d4cabd97e7b14ec76e52af99f924d531861102c
Thomas Feldmann <mail@tfeldmann.de>
2019-10-25 14:18:42 +0200

    make location of version string explicit

--- organize/cli.py
+++ organize/cli.py
@@ -31,7 +31,8 @@ import sys
 from colorama import Fore, Style
 from docopt import docopt
 
-from . import CONFIG_DIR, CONFIG_PATH, LOG_PATH, __version__
+from . import CONFIG_DIR, CONFIG_PATH, LOG_PATH
+from .__version__ import __version__
 from .compat import Path
 from .config import Config
 from .core import execute_rules
