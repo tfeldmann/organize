ef1d64e7d512f7d1a754716186cead1b967900d2
Thomas Feldmann <mail@tfeldmann.de>
2019-11-26 11:47:32 +0100

    add type hints

--- organize/filters/exif.py
+++ organize/filters/exif.py
@@ -1,8 +1,13 @@
 import collections
+from typing import Any, DefaultDict, Dict, Mapping, Optional, Union
 
-import exifread
+import exifread  # type: ignore
+
+from organize.compat import Path
 
 from .filter import Filter
+
+ExifDict = Mapping[str, Union[str, Mapping[str, str]]]
 
 
 class Exif(Filter):
@@ -11,23 +16,25 @@ class Exif(Filter):
     Filter by EXIF data
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: str, **kwargs: str) -> None:
         self.args = args  # expected exif keys
         self.kwargs = kwargs  # exif keys with expected values
 
-    def category_dict(self, tags):
-        result = collections.defaultdict(dict)
+    def category_dict(self, tags: Mapping[str, str]) -> ExifDict:
+        result = collections.defaultdict(dict)  # type: DefaultDict[str, Dict[str, str]]
         for key, value in tags.items():
             if " " in key:
                 category, field = key.split(" ", maxsplit=1)
                 result[category][field] = value
             else:
-                result[key] = value
+                result[key] = value  # type: ignore
         return result
 
-    def matches(self, path):
+    def matches(self, path: Path) -> Union[bool, ExifDict]:
+        # NOTE: This should return Union[Literal[False], ExifDict] but Literal is only
+        # available in Python 3.8.
         with path.open("rb") as f:
-            exiftags = exifread.process_file(f, details=False)
+            exiftags = exifread.process_file(f, details=False)  # type: Dict
         if not exiftags:
             return False
 
@@ -45,10 +52,11 @@ class Exif(Filter):
                 return False
         return self.category_dict(tags)
 
-    def pipeline(self, args):
-        tags = self.matches(args.path)
-        if tags:
+    def pipeline(self, args: Mapping[str, Any]) -> Optional[Dict[str, ExifDict]]:
+        tags = self.matches(args["path"])
+        if isinstance(tags, dict):
             return {"exif": tags}
+        return None
 
-    def __str__(self):
-        return "EXIF(%s)" % ", ".join(self.kwargs.items())
+    def __str__(self) -> str:
+        return "EXIF(%s)" % ", ".join("%s=%s" % (k, v) for k, v in self.kwargs.items())
