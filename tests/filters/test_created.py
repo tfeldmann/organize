fe460f910f1c0f77bfcef7d080325366f12275d5
Thomas Feldmann <mail@tfeldmann.de>
2020-03-04 15:46:31 +0100

    update tests

--- tests/filters/test_created.py
+++ tests/filters/test_created.py
@@ -1,26 +1,26 @@
-from datetime import datetime, timedelta
-
 from unittest.mock import patch
+
+import pendulum
 
 from organize.compat import Path
 from organize.filters import Created
 
 
 def test_min():
-    now = datetime.now()
+    now = pendulum.now()
     created = Created(days=10, hours=12, mode="older")
     with patch.object(created, "_created") as mock_cr:
-        mock_cr.return_value = now - timedelta(days=10, hours=0)
+        mock_cr.return_value = now - pendulum.duration(days=10, hours=0)
         assert created.run(path=Path("~")) is None
-        mock_cr.return_value = now - timedelta(days=10, hours=13)
+        mock_cr.return_value = now - pendulum.duration(days=10, hours=13)
         assert created.run(path=Path("~"))
 
 
 def test_max():
-    now = datetime.now()
+    now = pendulum.now()
     created = Created(days=10, hours=12, mode="newer")
     with patch.object(created, "_created") as mock_cr:
-        mock_cr.return_value = now - timedelta(days=10, hours=0)
+        mock_cr.return_value = now - pendulum.duration(days=10, hours=0)
         assert created.run(path=Path("~"))
-        mock_cr.return_value = now - timedelta(days=10, hours=13)
+        mock_cr.return_value = now - pendulum.duration(days=10, hours=13)
         assert created.run(path=Path("~")) is None
