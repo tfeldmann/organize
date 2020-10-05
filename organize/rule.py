9af2e22669de2aac6934ee3bad785a200f69495f
Thomas Feldmann <mail@tfeldmann.de>
2020-10-05 20:44:49 +0200

    create RuleBook and default hierarchy

--- organize/rule.py
+++ organize/rule.py
@@ -1,11 +1,16 @@
+"""
+https://github.com/tfeldmann/organize/pull/85
+"""
 import logging
 import re
+from collections import namedtuple
 from copy import deepcopy
+from os.path import abspath, expanduser, expandvars
 from pathlib import Path
-from typing import Iterable, Iterator, Text, Tuple, Mapping, Optional
+from typing import Optional
 
 import fs
-from fs.walk import Walker
+
 from .glob import Globber
 from .output import console_output
 from .utils import DotDict
@@ -13,178 +18,166 @@ from .utils import DotDict
 WILDCARD_REGEX = re.compile(r"(?<!\\)[\*\?\[]+")
 logger = logging.getLogger(__name__)
 
+GlobItem = namedtuple("GlobItem", "label base_fs base_dir path")
+
+DEFAULT_OPTIONS = {
+    "exclude_dirnames": (),
+    "exclude_filenames": (),
+    "system_exclude_dirnames": ("*.git", "*.svn", ".venv", ".pio"),
+    "system_exclude_filenames": (
+        "thumbs.db",
+        "desktop.ini",
+        "~$*",
+        ".DS_Store",
+        ".localized",
+    ),
+    "max_depth": 0,
+    "search": "breadth",
+    "ignore_errors": False,
+    "case_sensitive": True,
+    "namespaces": "basic",
+    "expanduser": True,
+    "expandvars": True,
+    # TODO:
+    "group_by_folder": False,
+    "normalize_unicode": True,
+    "skip_handled_files": True,
+}
+
+
+def merge_options(options, defaults):
+    result = defaults.copy()
+    if options:
+        result.update(options)
+    return result
+
 
 class Folder:
-    """A Folder object globs a single folder for files or folders
-    """
+    """A Folder object globs a single folder for files or folders"""
 
-    def __init__(self, path="/", glob="*", *, base_fs="", **kwargs):
-        if not base_fs:
-            self.base_fs = "osfs://" + path
-            self.path = "/"
-        else:
-            self.base_fs = base_fs
-            self.path = path
-
+    def __init__(
+        self, path="/", glob="*", *, base_fs="osfs:///", options=None, defaults=None
+    ):
+        self.base_dir = str(path)
         self.glob = glob
-        self.kwargs = kwargs  # additional walker parameters
+        self.base_fs = base_fs
 
-    def files(self):
-        file_glob = self.glob.rstrip("/")
-        for filesystem, path, info in self._glob(file_glob):
-            if info.is_file:
-                yield filesystem, path
+        self._options = options or {}
+        self.defaults = defaults or DEFAULT_OPTIONS
 
-    def dirs(self, **kwargs):
-        dir_glob = fs.path.forcedir(self.glob)
-        for filesystem, path, info in self._glob(dir_glob):
-            if info.is_dir:
-                yield filesystem, path
+    def label(self):
+        if self.base_fs.startswith("osfs://"):
+            return self.base_dir
+        else:
+            return self.base_fs + self.base_dir if self.base_dir != "/" else ""
 
     def _glob(self, pattern):
+        """ execute the given glob pattern """
+        options = merge_options(self._options, self.defaults)
+
+        path = self.base_dir
+        if options["expanduser"]:
+            path = expanduser(path)
+        if options["expandvars"]:
+            path = expandvars(path)
+        path = abspath(path)
+
+        globber_kwargs = dict(
+            pattern=pattern,
+            path=path,
+            max_depth=options["max_depth"],
+            case_sensitive=options["case_sensitive"],
+            exclude_dirs=(
+                options["exclude_dirnames"] + options["system_exclude_dirnames"]
+            ),
+            exclude_files=(
+                options["exclude_filenames"] + options["system_exclude_filenames"]
+            ),
+            search=options["search"],
+            ignore_errors=options["ignore_errors"],
+            namespaces=options["namespaces"],
+        )
         with fs.open_fs(self.base_fs) as glob_fs:
-            for path, info in Globber(
-                fs=glob_fs, pattern=pattern, path=self.path, **self.kwargs
-            ):
+            for path, info in Globber(fs=glob_fs, **globber_kwargs):
                 yield glob_fs, path, info
 
-    @classmethod
-    def from_string(cls, string):
-        raise NotImplementedError()  # TODO
-
-
-class FolderWalker:
-    """A FolderWalker globs multiple given folders for files or folders.
-    """
-
-    def __init__(self, folders, optimize=True):
-        self.folders = folders
-
     def files(self):
-        for folder in self.folders:
-            yield from folder.files()
+        label = self.label()
+        file_glob = self.glob.rstrip("/")
+        for filesystem, path, info in self._glob(pattern=file_glob):
+            if info.is_file:
+                yield GlobItem(
+                    label=label,
+                    base_fs=filesystem,
+                    base_dir=self.base_dir,
+                    path=path,
+                )
 
-    def dirs(self):
-        for folder in self.folders:
-            yield from folder.dirs()
+    def dirs(self, **kwargs):
+        label = self.label()
+        dir_glob = fs.path.forcedir(self.glob)
+        for filesystem, path, info in self._glob(pattern=dir_glob):
+            if info.is_dir:
+                yield GlobItem(
+                    label=label,
+                    base_fs=filesystem,
+                    base_dir=self.base_dir,
+                    path=path,
+                )
 
+    @classmethod
+    def from_string(cls, pattern: str) -> "Folder":
+        """
+        Allowed pattern formats:
+            - zip://./Archiv.zip/test/**/test.*
+            - Office/2019/
+            - Office/201*/*
 
-"""
-Input:
-    osfs://~/Documents/
-    osfs://~/Pictures
-    ! osfs://~/Pictures/Old/*.png
-    osfs://~/Pictures/Old/*_include.png
-    osfs://~/Pictures/Additional
-    osfs://~/Pictures/2020-*/*.png
-    osfs://~/Downloads/Folder1/**/*.*
-    osfs://~/Downloads/Folder2/**/*
-
-
-Entrypoints (group by base, then group by path elements):
-- osfs://~/Documents/
-    - *
-- osfs://~/Pictures/
-    - /Old/
-        - ! *.png
-        - *_include.png
-    - /Additional/
-    - /2020-*/
-        - *.png
-- osfs://~/Downloads/Folder1
-    - **/*.*
-- osfs://~/Downloads/Folder2
-    - **/*
-"""
-
-
-def create_file_glob(pattern: Text) -> Tuple[Text, Text]:
-    # If the glob pattern ends in a /, it will only match directory paths, otherwise it
-    # will match files and directories.
-    url = ""
-    index = pattern.find("://")  # find filesystem urls
-    if index > 0:
-        url, pattern = pattern[: index + 3], pattern[index + 3 :]
-    base, glob = [], []
-    force_glob = False
-    for component in fs.path.iteratepath(pattern):
-        if force_glob or WILDCARD_REGEX.search(component):
-            glob.append(component)
-            force_glob = True
-        else:
-            base.append(component)
-
-    return (url + fs.path.join(*base), fs.path.join(*glob))
+        returns:
+            A `Folder` instance.
+        """
+        # TODO: base_fs detection for zip files.
+        # If the glob pattern ends in a /, it will only match directory paths, otherwise
+        # it will match files and directories.
+        url = ""
+        index = pattern.find("://")  # find filesystem urls
+        if index > 0:
+            url, pattern = pattern[: index + 3], pattern[index + 3 :]
+        base, glob = [], []
+        force_glob = False
+        for component in fs.path.iteratepath(pattern):
+            if force_glob or WILDCARD_REGEX.search(component):
+                glob.append(component)
+                force_glob = True
+            else:
+                base.append(component)
+        return cls(path=(url + fs.path.join(*base)), glob=fs.path.join(*glob))
 
 
 class Rule:
-    def __init__(self, folders=None, filters=None, actions=None, **kwargs):
+    def __init__(
+        self, folders=None, filters=None, actions=None, options=None, defaults=None
+    ):
         self.folders = folders or []
         self.filters = filters or []
         self.actions = actions or []
+        self._options = options or {}
+        self.defaults = defaults or DEFAULT_OPTIONS
 
-        self.config = {
-            "exclude_dirnames": (),
-            "exclude_filenames": (),
-            "system_exclude_dirnames": ("*.git", "*.svn", ".venv", ".pio"),
-            "system_exclude_filenames": (
-                "thumbs.db",
-                "desktop.ini",
-                "~$*",
-                ".DS_Store",
-                ".localized",
-            ),
-            "max_depth": 0,
-            "search": "breadth",
-            "ignore_errors": False,
-            "case_sensitive": True,
-            # TODO: "group_by_folder": False,  - only possible in RuleGrouper
-            # TODO: "normalize_unicode": True,
-            # TODO: "case_sensitive": True,
-        }
-        self.config.update(kwargs)
+    def options(self):
+        return merge_options(self._options, self.defaults)
 
-    def walker_settings(self, pattern, args) -> Tuple[Text, Mapping]:
-        parsed_pattern = parse_pattern(pattern)  # type: Pattern
+    def files(self):
+        options = self.options()
+        for folder in self.folders:
+            folder.defaults = options
+            yield from folder.files()
 
-        # if a recursive glob pattern and not settings are given, adjust max_depth
-        if "max_depth" not in args:
-            args["max_depth"] = parsed_pattern.max_depth
-
-        # combine defaults and folder config
-        config = self.config.copy()
-        config.update(args)
-
-        walker_conf = dict(
-            filter=parsed_pattern.glob_files,
-            filter_dirs=parsed_pattern.glob_dirs,
-            exclude=list(
-                set(config["system_exclude_files"]) | set(config["exclude_files"])
-            ),
-            exclude_dirs=list(
-                set(config["system_exclude_dirs"]) | set(config["exclude_dirs"])
-            ),
-            ignore_errors=config["ignore_errors"],
-            max_depth=config["max_depth"],
-            search=config["search"],
-        )
-        return (parsed_pattern.base, walker_conf)
-
-    def walkers(self) -> Iterable[Tuple[Text, Walker]]:
-        for entry in self.folders:
-            if isinstance(entry, str):
-                pattern, args = entry, {}
-            else:
-                pattern, args = entry
-            folder, conf = self.walker_settings(pattern=pattern, args=args)
-            walker = Walker(**conf)
-            yield (folder, walker)
-
-    def files(self) -> Iterator[Tuple[Text, Text]]:
-        for path, walker in self.walkers():
-            folder_fs = fs.open_fs(path)
-            for f in walker.files(folder_fs):
-                yield (path, f)
+    def dirs(self):
+        options = self.options()
+        for folder in self.folders:
+            folder.defaults = options
+            yield from folder.dirs()
 
     def filter_pipeline(self, args: DotDict) -> bool:
         for filter_ in self.filters:
@@ -215,27 +208,27 @@ class Rule:
                 return False
         return True
 
-    def run_single(self, path, basedir, simulate=True) -> Optional[bool]:
+    def pipeline(self, item: GlobItem, simulate=True) -> Optional[bool]:
         # args will be modified in place by action and filter pipeline
         args = DotDict(
-            path=Path(fs.path.combine(basedir, path)),
-            basedir=Path(basedir),
-            relative_path=Path(path),
+            basedir=item.base_dir,
+            path=Path(fs.path.combine(item.base_dir, item.path)),
+            relative_path=Path(fs.path.relativefrom(item.base_dir, item.path)),
             simulate=simulate,
         )
-        console_output.set_location(args.basedir, args.relative_path)
+        console_output.set_location(item.label, args.relative_path)
         match = self.filter_pipeline(args)
         if match:
             return self.action_pipeline(args)
         return None
 
-    def run(self, simulate=True):
+    def run(self, simulate=False):
         if simulate:
             console_output.simulation_banner()
 
         count = [0, 0]
-        for base, path in self.files():
-            success = self.run_single(path=path, basedir=base, simulate=simulate)
+        for item in self.files():
+            success = self.pipeline(item=item, simulate=simulate)
             if success is not None:
                 count[success] += 1
         failed, succeded = count
@@ -249,16 +242,50 @@ class Rule:
 
         return failed > 0
 
+    def sim(self):
+        self.run(simulate=True)
+
+
+class RuleBook:
+    def __init__(self, rules=None, options=None, defaults=None):
+        self.rules = rules or []
+        self._options = options or {}
+        self.defaults = defaults or DEFAULT_OPTIONS
+
+    def run(self, simulate=False):
+        options = merge_options(self._options, self.defaults)
+        for rule in self.rules:
+            rule.defaults = options
+            rule.run(simulate=simulate)
+
+    def sim(self):
+        self.run(simulate=True)
+
 
 def test():
     from . import actions, filters
 
-    rule = Rule(
-        folders=[Folder(path="~/Documents", glob="**/*")],
-        filters=[filters.Extension("pdf"),],
-        actions=[actions.Echo("{path}")],
+    book = RuleBook(
+        [
+            Rule(
+                folders=[
+                    Folder(
+                        path="~/Desktop",
+                        glob="**/*",
+                    ),
+                    Folder(
+                        base_fs="zip://testzip.zip",
+                        path="/",
+                        glob="**/*",
+                    ),
+                ],
+                filters=[filters.Extension()],
+                actions=[actions.Echo("{extension.upper}")],
+            )
+        ],
+        options={"max_depth": 1},
     )
-    rule.run(simulate=True)
+    book.sim()
 
 
 if __name__ == "__main__":
