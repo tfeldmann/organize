9b0afab57875889dde85fb7e5168701c18c9d183
Thomas Feldmann <mail@tfeldmann.de>
2022-01-25 20:56:58 +0100

    add empty filter

---
+++ organize/filters/empty.py
@@ -0,0 +1,25 @@
+from fs.base import FS
+
+from .filter import Filter
+
+
+class Empty(Filter):
+
+    """Only lets through empty dirs and folders"""
+
+    name = "empty"
+
+    @classmethod
+    def get_schema(cls):
+        return cls.name
+
+    def pipeline(self, args: dict):
+        fs = args["fs"]  # type: FS
+        fs_path = args["fs_path"]  # type: str
+
+        if fs.isdir(fs_path):
+            return fs.isempty(fs_path)
+        return fs.getsize(fs_path) == 0
+
+    def __str__(self) -> str:
+        return "Empty()"
