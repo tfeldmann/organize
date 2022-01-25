52ebf0121e94d96d55a442ee8a01ae516fe447b7
Thomas Feldmann <mail@tfeldmann.de>
2022-01-25 15:23:18 +0100

    update hash docs

--- organize/filters/hash.py
+++ organize/filters/hash.py
@@ -11,6 +11,23 @@ logger = logging.getLogger(__name__)
 
 class Hash(Filter):
 
+    """
+    Args:
+        algorithm (str)
+        Any hashing algorithm available to python's `hashlib`.
+
+        List the available algorithms on your installation:
+
+        >>> import hashlib
+        >>> hashlib.algorithms_guaranteed
+        {'shake_256', 'sha3_256', 'sha1', 'sha3_224', 'sha384', 'sha512', 'blake2b', 'blake2s', 'sha256', 'sha224', 'shake_128', 'sha3_512', 'sha3_384', 'md5'}
+        >>> hashlib.algorithms_available
+        {'shake_256', 'whirlpool', 'mdc2', 'blake2s', 'sha224', 'shake_128', 'sha3_512', 'sha3_224', 'sha384', 'md5', 'sha1', 'sha512_256', 'blake2b', 'sha256', 'sha512_224', 'ripemd160', 'sha3_384', 'md4', 'sm3', 'sha3_256', 'md5-sha1', 'sha512'}
+
+    Returns:
+        {"hash": str} where str is the hash of the file.
+    """
+
     name = "hash"
 
     def __init__(self, algorithm="md5"):
