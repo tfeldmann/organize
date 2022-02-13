7c1b7ed35347c079c38e2abee46be17cea2d8eb3
Thomas Feldmann <mail@tfeldmann.de>
2022-02-13 11:02:13 +0100

    fix #188

--- organize/filters/filecontent.py
+++ organize/filters/filecontent.py
@@ -1,15 +1,13 @@
+import logging
 import re
-from typing import Any, Dict, Mapping, Optional
+from typing import Any
 
 from fs.base import FS
 from fs.errors import NoSysPath
 
 from .filter import Filter, FilterResult
 
-SUPPORTED_EXTENSIONS = (
-    # not supported: .gif, .jpg, .mp3, .ogg, .png, .tiff, .wav
-    ".csv .doc .docx .eml .epub .json .html .msg .odt .pdf .pptx .ps .rtf .txt .xlsx .xls"
-).split()
+logger = logging.getLogger(__name__)
 
 
 class FileContent(Filter):
@@ -33,40 +31,41 @@ class FileContent(Filter):
     def __init__(self, expr="(?P<all>.*)") -> None:
         self.expr = re.compile(expr, re.MULTILINE | re.DOTALL)
 
-    def matches(self, path: str, extension: str) -> Any:
-        if extension not in SUPPORTED_EXTENSIONS:
-            return
+    def matches(self, path: str) -> Any:
         try:
             import textract
 
             content = textract.process(
                 str(path),
-                extension=extension,
                 errors="ignore",
             )
-            return self.expr.search(content.decode("utf-8", errors="ignore"))
+            match = self.expr.search(content.decode("utf-8", errors="ignore"))
+            return match
         except ImportError as e:
             raise ImportError(
                 "textract is not installed. "
                 "Install with pip install organize-tool[textract]"
             ) from e
-        except textract.exceptions.CommandLineError:
-            pass
+        except textract.exceptions.CommandLineError as e:
+            logger.warning("CommandLineError %", e)
 
     def pipeline(self, args: dict) -> FilterResult:
         fs = args["fs"]  # type: FS
         fs_path = args["fs_path"]
         if fs.isdir(fs_path):
             raise ValueError("Dirs not supported")
-        extension = fs.getinfo(fs_path).suffix
         try:
             syspath = fs.getsyspath(fs_path)
         except NoSysPath as e:
             raise EnvironmentError(
                 "filecontent only supports the local filesystem"
             ) from e
-        match = self.matches(path=syspath, extension=extension)
+        match = self.matches(path=syspath)
+        if match:
+            updates = {self.get_name(): match.groupdict()}
+        else:
+            updates = {}
         return FilterResult(
             matches=bool(match),
-            updates={self.get_name(): match.groupdict()},
+            updates=updates,
         )
