ba44eef5d56ffbefe6af5fe5726192ca01d88f9f
Thomas Feldmann <mail@tfeldmann.de>
2022-08-18 15:58:33 +0200

    got the hybrid approach working

--- organize/config_pydantic.py
+++ organize/config_pydantic.py
@@ -1,7 +1,8 @@
 from rich import print
 from enum import Enum
-from typing import Annotated, List, Literal, Optional, Union
-
+from fs.base import FS
+from typing import List, Union
+from typing_extensions import Annotated, Literal
 from pydantic import BaseModel as PydanticBaseModel
 from pydantic import Field, root_validator, validator
 
@@ -29,35 +30,62 @@ class BaseModel(PydanticBaseModel):
         underscore_attrs_are_private = True
 
 
+DEFAULT_SYSTEM_EXCLUDE_FILES = ["asd"]
+DEFAULT_SYSTEM_EXCLUDE_DIRS = [".git"]
+
+
 class Location(BaseModel):
     path: str
-    max_depth: Optional[int] = None
+    max_depth: Union[int, None] = None
     search: LocationSearchMethod = LocationSearchMethod.depth
-    exclude_files: Union[None, str, List[str]]
-    exclude_dirs: Union[None, str, List[str]]
-    system_exclude_files: Union[None, str, List[str]]
-    system_exclude_dirs: Union[None, str, List[str]]
-    filter: Union[None, str, List[str]]
-    filter_dirs: Union[None, str, List[str]]
+    exclude_files: List[str] = Field(default_factory=list)
+    exclude_dirs: List[str] = Field(default_factory=list)
+    system_exclude_files: List[str] = Field(
+        default_factory=lambda: DEFAULT_SYSTEM_EXCLUDE_FILES
+    )
+    system_exclude_dirs: List[str] = Field(
+        default_factory=lambda: DEFAULT_SYSTEM_EXCLUDE_DIRS
+    )
+    filter: List[str] = Field(default_factory=list)
+    filter_dirs: List[str] = Field(default_factory=list)
     ignore_errors: bool = False
-    filesystem: Optional[Union[str, object]]
+    filesystem: Union[str, FS] = "."
+
+    @validator(
+        "exclude_files",
+        "exclude_dirs",
+        "system_exclude_files",
+        "system_exclude_dirs",
+        pre=True,
+    )
+    def ensure_set(cls, value):
+        if isinstance(value, str):
+            return set([value])
+        return value
 
 
 # FILTER
 
-FilterType = None
+FilterType = str
 
 # ACTIONS
 
 
 class Action(BaseModel):
     class Meta:
-        param_for_single_str = None
+        accepts_positional_arg = None
+
+    def __init__(self, *args, **kwargs) -> None:
+        if self.Meta.accepts_positional_arg and len(args) == 1:
+            kwargs[self.Meta.accepts_positional_arg] = args[0]
+            super().__init__(**kwargs)
+            return
+        super().__init__(*args, **kwargs)
 
     @root_validator(pre=True)
     def handle_single_str(cls, value):
         if "__non_dict_arg__" in value:
-            param = cls.Meta.param_for_single_str
+            param = cls.Meta.accepts_positional_arg
             if not param:
                 raise ValueError("Non-dict arguments are not accepted")
             param_val = value.pop("__non_dict_arg__")
@@ -71,7 +99,7 @@ class ConflictOptions(str, Enum):
 
 
 class Move(Action):
-    name: Literal["move"]
+    name: Literal["move"] = "move"
 
     dest: str
     on_conflict: ConflictOptions = ConflictOptions.rename_new
@@ -79,11 +107,11 @@ class Move(Action):
     filesystem: Union[None, str] = None
 
     class Meta:
-        param_for_single_str = "dest"
+        accepts_positional_arg = "dest"
 
 
 class Copy(Action):
-    name: Literal["copy"]
+    name: Literal["copy"] = "copy"
 
     dest: str
     on_conflict: str = "rename_new"
@@ -93,9 +121,12 @@ class Copy(Action):
 
 ## -- end actions
 
-ActionType = Annotated[
-    Union[Move, Copy],
-    Field(discriminator="name"),
+ActionType = Union[
+    Action,
+    Annotated[
+        Union[Move, Copy],
+        Field(discriminator="name"),
+    ],
 ]
 
 
@@ -120,7 +151,7 @@ def expand_location(x: Union[str, list]):
 
 class Rule(BaseModel):
     name: str = "Rule"
-    enabled: bool = True
+    enabled: bool = Field(True, repr=False)
     subfolders: bool = False
     filter_mode: RuleFilterMode = RuleFilterMode.all
     targets: RuleTarget = RuleTarget.files
@@ -147,7 +178,7 @@ class Rule(BaseModel):
             if not isinstance(args, dict):
                 return {"name": name, "__non_dict_arg__": args}
             return {"name": name, **args}
-        raise ValueError("actions must be a list of dicts: %s" % value)
+        return value
 
     @validator("filters", pre=True, each_item=True)
     def validate_filters(cls, v):
@@ -172,6 +203,10 @@ class Config(BaseModel):
 
 
 if __name__ == "__main__":
+    import sys, inspect
+
+    sys.exit()
+
     x = Rule.parse_obj(
         {
             "locations": [{"path": "."}],
@@ -182,4 +217,13 @@ if __name__ == "__main__":
         print(action)
 
     print(x)
-    print(x.dict())
+    print(x.json())
+
+    print(
+        Rule(
+            locations=Location(path="asd", exclude_dirs="asd"),
+            actions=[Move(dest="asd"), {"move": "test"}],
+            filters=[],
+            targets="dirs",
+        ).dict()
+    )
