b9cc98547a4df68780974734ea772b61eab48179
Thomas Feldmann <mail@tfeldmann.de>
2022-08-18 18:13:36 +0200

    refactor utils

--- organize/filters/_timefilter.py
+++ organize/filters/_timefilter.py
@@ -4,7 +4,17 @@ from typing import Union
 from schema import Optional, Or
 
 from .filter import Filter, FilterResult
-from .utils import age_condition_applies
+
+
+def age_condition_applies(dt: datetime, age: timedelta, mode: str, reference: datetime):
+    """
+    Returns whether `dt` is older / newer (`mode`) than `age` as measured on `reference`
+    """
+    if mode not in ("older", "newer"):
+        raise ValueError(mode)
+
+    is_past = (dt + age).timestamp() < reference.timestamp()
+    return (mode == "older") == is_past
 
 
 class TimeFilter(Filter):
