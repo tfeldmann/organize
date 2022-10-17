1994cf846e412c830706453a8d2fd0f927ae39eb
Thomas Feldmann <mail@tfeldmann.de>
2022-10-17 11:29:07 +0200

    add photo sorting test

--- tests/filters/test_lastmodified.py
+++ tests/filters/test_lastmodified.py
@@ -1,5 +1,9 @@
 from datetime import datetime, timedelta
 
+from conftest import read_files
+from fs.base import FS
+
+from organize import core
 from organize.filters import LastModified
 
 
@@ -15,3 +19,36 @@ def test_max():
     lm = LastModified(days=10, hours=12, mode="newer")
     assert lm.matches_datetime(now - timedelta(days=10, hours=0))
     assert not lm.matches_datetime(now - timedelta(days=10, hours=13))
+
+
+def test_photo_sorting(testfs: FS):
+    conf = """
+    rules:
+      - locations: "."
+        subfolders: true
+        filters:
+         - lastmodified
+        actions:
+         - move:
+            dest: "./pics/{lastmodified.strftime('%Y/%m/%d')}/"
+            on_conflict: skip
+    """
+    testfs.touch("photo1")
+    testfs.touch("photo2")
+    testfs.touch("photo3")
+    testfs.setinfo(
+        "photo1", {"details": {"modified": datetime(2000, 1, 12).timestamp()}}
+    )
+    testfs.setinfo(
+        "photo2", {"details": {"modified": datetime(2020, 1, 1).timestamp()}}
+    )
+    testfs.setinfo(
+        "photo3", {"details": {"modified": datetime(2020, 1, 1).timestamp()}}
+    )
+    core.run(conf, simulate=False, working_dir=testfs)
+    assert read_files(testfs) == {
+        "pics": {
+            "2000": {"01": {"12": {"photo1": ""}}},
+            "2020": {"01": {"01": {"photo2": "", "photo3": ""}}},
+        }
+    }
