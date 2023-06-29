e2dd6653ae235d0b4120fa7d36835418d2e3e82e
Florian Fritz <mehr.fleisch@web.de>
2023-06-29 13:42:23 +0200

    Added tests for datetime conversion

--- tests/filters/test_exif.py
+++ tests/filters/test_exif.py
@@ -70,9 +70,57 @@ def test_exif_filter_tag_exists(images_folder):
     run(config, simulate=False, working_dir=images_folder)
     out = images_folder.readtext("out.txt")
     assert len(out.splitlines()) == 3
-    assert "4: 2018:02:22" in out
-    assert "5: 2015:07:08" in out
-    assert "3: 2017:08:12" in out
+    assert "4: 2018-02-22" in out
+    assert "5: 2015-07-08" in out
+    assert "3: 2017-08-12" in out
+
+
+def test_exif_filter_datetime(images_folder):
+    config = """
+      rules:
+        - locations: "/"
+          filters:
+            - name
+            - exif:
+                exif.datetimeoriginal
+          actions:
+            - write:
+                text: "{name}: {exif.exif.datetimeoriginal.isoformat()}"
+                path: "out.txt"
+    """
+    run(config, simulate=False, working_dir=images_folder)
+    out = images_folder.readtext("out.txt")
+    assert len(out.splitlines()) == 5
+    # 1: EXIF DateTimeOriginal 2018:09:29 18:52:46
+    assert "1: 2018-09-29T18:52:46" in out
+    # 2: EXIF DateTimeOriginal 2018:04:29 16:24:21
+    assert "2: 2018-04-29T16:24:21" in out
+    # 3: DateTimeOriginal 2017:08:12 12:23:36
+    assert "3: 2017-08-12T12:23:36" in out
+    # 4: EXIF DateTimeOriginal 2018:02:22 09:34:41
+    assert "4: 2018-02-22T09:34:41" in out
+    # 5: EXIF DateTimeOriginal 2015:07:08 14:43:52
+    assert "5: 2015-07-08T14:43:52" in out
+
+
+def test_exif_filter_timezone(images_folder):
+    config = """
+      rules:
+        - locations: "/"
+          filters:
+            - name
+            - exif:
+                exif.offsettimeoriginal
+          actions:
+            - write:
+                text: "{name}: {exif.exif.offsettimeoriginal}"
+                path: "out.txt"
+    """
+    run(config, simulate=False, working_dir=images_folder)
+    out = images_folder.readtext("out.txt")
+    assert len(out.splitlines()) == 1
+    # 1: EXIF OffsetTimeOriginal +01:00
+    assert "1: UTC+01:00" in out
 
 
 def test_exif_filter_by_multiple_keys(images_folder):
