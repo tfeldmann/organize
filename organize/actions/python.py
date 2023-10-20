ba547bba5d1c23b3d13b05061cffb80009fd070a
Thomas Feldmann <mail@tfeldmann.de>
2023-10-20 19:45:47 +0200

    fix python action

--- organize/actions/python.py
+++ organize/actions/python.py
@@ -1,11 +1,12 @@
 import textwrap
-from typing import ClassVar, Iterable
+from typing import ClassVar, Dict, Optional
 
 from pydantic.dataclasses import dataclass
 
 from organize.action import ActionConfig
 from organize.output import Output
 from organize.resource import Resource
+from organize.utils import deep_merge
 
 
 @dataclass
@@ -32,24 +33,34 @@ class Python:
     def __post_init__(self):
         self.code = textwrap.dedent(self.code)
 
-    def usercode(self, *args, **kwargs):
-        pass  # will be overwritten by `create_method`
-
-    def create_method(self, name: str, argnames: Iterable[str], code: str) -> None:
-        globals_ = globals().copy()
-        globals_["print"] = self.print
-        locals_ = locals().copy()
-        funccode = "def {fnc}__({arg}):\n{cod}\n\nself.{fnc} = {fnc}__\n".format(
-            fnc=name,
-            arg=", ".join(argnames),
-            cod=textwrap.indent(textwrap.dedent(code), " " * 4),
-        )
-        exec(funccode, globals_, locals_)  # pylint: disable=exec-used
+    def __usercode__(self, print, **kwargs) -> Optional[Dict]:
+        raise NotImplementedError()
 
     def pipeline(self, res: Resource, output: Output, simulate: bool):
         if simulate and not self.run_in_simulation:
             output.msg(res=res, msg="** Code not run in simulation. **", level="warn")
             return
 
-        self.create_method(name="usercode", argnames=res.dict().keys(), code=self.code)
-        res.vars.update(self.usercode(**res.dict()))
+        def _output_msg(*values, sep: str = " ", end: str = ""):
+            """
+            the print function for the use code needs to print via the current output
+            """
+            output.msg(res=res, msg=f"{sep.join(values)}{end}", sender=self)
+
+        # codegen the user function with arguments as available in the resource
+        kwargs = ", ".join(res.dict().keys())
+        func = f"def __usercode(print, {kwargs}):\n"
+        func += textwrap.indent(self.code, "    ")
+        func += "\n\nself.__usercode__ = __usercode"
+        exec(func, globals().copy(), locals().copy())
+        result = self.__usercode__(print=_output_msg, **res.dict())
+
+        # deep merge the resulting dict
+        if not (result is None or isinstance(result, dict)):
+            raise ValueError("The python code must return None or a dict")
+
+        if isinstance(result, dict):
+            res.vars[self.action_config.name] = deep_merge(
+                res.vars.get(self.action_config.name, dict()),
+                result,
+            )
