44233e19253eb21d27aababb648f612f77d0397a
Thomas Feldmann <mail@tfeldmann.de>
2023-11-15 22:56:18 +0100

    update imports

--- organize/__init__.py
+++ organize/__init__.py
@@ -1,8 +1,10 @@
-from .config import Config, ConfigError
+from .config import Config
+from .errors import ConfigError, ConfigNotFound
 from .rule import Rule
 
 __all__ = (
     "Config",
     "ConfigError",
+    "ConfigNotFound",
     "Rule",
 )
