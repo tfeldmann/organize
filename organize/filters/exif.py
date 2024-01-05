342a418557111ea34456df104482ca029b5751d6
Thomas Feldmann <mail@tfeldmann.de>
2024-01-05 15:17:24 +0100

    add exiftool support

--- organize/filters/exif.py
+++ organize/filters/exif.py
@@ -1,6 +1,11 @@
 import collections
 import fnmatch
+import json
+import logging
+import os
+import subprocess
 from datetime import date, datetime, timedelta
+from functools import lru_cache
 from pathlib import Path
 from typing import Any, ClassVar, DefaultDict, Dict, Optional, Union
 
@@ -13,64 +18,151 @@ from organize.output import Output
 from organize.resource import Resource
 
 ExifValue = Union[str, datetime, date, timedelta]
-ExifDict = Dict[str, Union[ExifValue, Dict[str, ExifValue]]]
-ExifDefaultDict = DefaultDict[str, Union[ExifValue, DefaultDict[str, ExifValue]]]
+ExifDict = Dict[str, Dict[str, ExifValue]]
+ExifDefaultDict = DefaultDict[str, DefaultDict[str, ExifValue]]
+
+ORGANIZE_EXIFTOOL_PATH = os.environ.get("ORGANIZE_EXIFTOOL_PATH", "")
 
 
-def parse_tag(key: str, value: str) -> ExifValue:
-    """
-    Try to parse `value` for the given `key`.
+@lru_cache(maxsize=1)
+def exiftool_available() -> bool:
+    # don't use exiftool if user blanked the env path
+    if not ORGANIZE_EXIFTOOL_PATH:
+        return False
 
-    - If `key` contains "datetime" convert `value` to 'datetime.datetime'
-        (e.g. image.datetime, exif.datetimeoriginal, exif.datetimedigitized)
-    - If `key` contains "date" convert `value` to 'datetime.date'
-        (e.g. gps.date)
-    - If `key` contains "offsettime" convert `value` to 'datetime.timedelta'
-        (e.g. exif.offsettimeoriginal, exif.offsettimedigitized)
-    - Otherwise `value` is not converted and returned as is
-    """
-    _key = key.lower()
+    # check whether the given path is executable
     try:
-        if "datetime" in _key:
-            # value = "YYYY:MM:DD HH:MM:SS"
-            return datetime.strptime(value[:19], "%Y:%m:%d %H:%M:%S")
-        elif "date" in _key:
-            # value = "YYYY:MM:DD"
-            return datetime.strptime(value[:10], "%Y:%m:%d").date()
-        elif "offsettime" in _key:
-            # value = "+HHMM" or "+HH:MM[:SS]" or "UTC+HH:MM[:SS]"
-            if value[:3].upper() == "UTC":
-                # Remove UTC
-                value = value[3:]
-            return datetime.strptime(
-                value.replace(":", ""), "%z"
-            ).utcoffset() or timedelta(seconds=0)
-        return value
-    except Exception:
-        return value
+        subprocess.check_call(
+            [ORGANIZE_EXIFTOOL_PATH, "-ver"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT,
+        )
+        return True
+    except subprocess.CalledProcessError:
+        logging.warning("exiddftool not available. Falling back to exifread library.")
+        return False
 
 
-def parse_and_categorize(tags: Dict[str, str]) -> ExifDict:
+def group_keys_by_splitting(
+    data: Dict[str, Any],
+    delimiter: str = " ",
+) -> Dict[str, Dict[str, Any]]:
+    """
+    >>> group_keys_by_splitting({"cat a": 1, "cat b": 2, "x c": 1, "x d": 2})
+    {"cat": {"a": 1, "b": 2}, "x": {"c": 1, "d": 2}}
+    """
     result: DefaultDict[str, Any] = collections.defaultdict(dict)
-    for key, value in tags.items():
-        if " " in key:
-            category, field = key.split(" ", maxsplit=1)
-            result[category][field] = parse_tag(key=field, value=value)
+    for k, v in data.items():
+        if delimiter in k:
+            category, field = k.split(delimiter, maxsplit=1)
+            result[category][field] = v
         else:
-            result[key] = parse_tag(key=key, value=value)
+            result[k] = v
     return dict(result)
 
 
-def read_exif_data(path: Path) -> Dict[str, str]:
-    """
-    returns parsed and categorized exif data for the given path.
-    """
-    data = exifread.process_file(fh=path.open("rb"), details=False, debug=False)
-    printable = {key.lower(): val.printable for (key, val) in data.items()}
-    return printable
+def lowercase_keys_recursive(data) -> Dict:
+    if isinstance(data, Dict):
+        return {k.lower(): lowercase_keys_recursive(v) for k, v in data.items()}
+    return data
 
 
-def matches_tags(filter_tags: Dict[str, Optional[str]], data: ExifDict) -> bool:
+def parse_date_value(value: str) -> Union[datetime, date, str]:
+    """
+    Parse datetime or date values (e.g. image.datetime, exif.datetimeoriginal, ...)
+    Exiftool often gives date and time values for keys with only "date" in their name
+    so we have to check both in order to preserve this information.
+    """
+    try:
+        return datetime.strptime(value[:19], "%Y:%m:%d %H:%M:%S")
+    except ValueError:
+        pass
+    try:
+        return datetime.strptime(value[:10], "%Y:%m:%d").date()
+    except ValueError:
+        pass
+    return value
+
+
+def parse_offset_value(value: str) -> Union[timedelta, str]:
+    """
+    Parse offset values (e.g. exif.offsettimeoriginal, exif.offsettimedigitized)
+    Supports formats "+HHMM" or "+HH:MM[:SS]" or "UTC+HH:MM[:SS]"
+    """
+    try:
+        if value[:3].upper() == "UTC":
+            value = value[3:]  # remove UTC
+        return datetime.strptime(value.replace(":", ""), "%z").utcoffset() or timedelta(
+            seconds=0
+        )
+    except (ValueError, TypeError):
+        return value
+
+
+def convert_value(key: str, value: str) -> ExifValue:
+    _key = key.lower()
+    if "date" in _key:
+        return parse_date_value(value)
+    if "offset" in _key:
+        return parse_offset_value(value)
+    return value
+
+
+def convert_recursive(data):
+    result = dict()
+    for k, v in data.items():
+        if isinstance(v, Dict):
+            result[k] = convert_recursive(v)
+        else:
+            result[k] = convert_value(k, v)
+    return result
+
+
+def exifread_read(path: Path) -> ExifDict:
+    """
+    Uses the `exifread` library to read the EXIF data
+    """
+    with path.open("rb") as f:
+        data = exifread.process_file(fh=f, details=False, debug=False)
+    # at this point data still contains exifread specific types like
+    # Short / Ratio / ASCII which we now convert to a printable representation
+    printable = {key: val.printable for (key, val) in data.items()}
+    grouped = group_keys_by_splitting(printable)
+    return grouped
+
+
+def exiftool_read(path: Path) -> ExifDict:
+    """
+    Uses the `exiftool` tool by Phil Harvey to read the EXIF data
+    """
+    try:
+        data_json = subprocess.check_output(
+            (
+                ORGANIZE_EXIFTOOL_PATH,
+                "-j",
+                "-g",
+                "--fast",
+                str(path),
+            ),
+            text=True,
+        )
+    except subprocess.CalledProcessError:
+        return dict()
+
+    # we pass a single filepath, so we are interested in the first element
+    data: Dict = json.loads(data_json)[0]
+
+    # if data contains no media specific EXIF keys we assume we don't have
+    # any relevant EXIF data.
+    if "EXIF" not in data.keys():
+        return dict()
+    return data
+
+
+def matches_tags(
+    filter_tags: Dict[str, Optional[str]],
+    data: Dict[str, Dict[str, str]],
+) -> bool:
     if not data:
         return False
     for k, v in filter_tags.items():
@@ -94,12 +186,24 @@ class Exif(BaseModel):
     The `exif` filter can be used as a filter as well as a way to get exif information
     into your actions.
 
+    By default this library uses the `exifread` library. If your image format is not
+    supported you can install `exiftool` (exiftool.org) and set the environment variable:
+
+    ```
+    ORGANIZE_EXIFTOOL_PATH="exiftool"
+    ```
+
+    organize will then use `exiftool` to extract the EXIF data.
+
     Exif fields which contain "datetime", "date" or "offsettime" in their fieldname
     will have their value converted to 'datetime.datetime', 'datetime.date' and
     'datetime.timedelta' respectivly.
     - `datetime.datetime` : exif.image.datetime, exif.exif.datetimeoriginal, ...
     - `datetime.date` : exif.gps.date, ...
     - `datetime.timedelta` : exif.exif.offsettimeoriginal, exif.exif.offsettimedigitized, ...
+
+    Attributes:
+        lowercase_keys (bool): Whether to lowercase all EXIF keys (Default: true)
 
     :returns:
         ``{exif}`` -- a dict of all the collected exif inforamtion available in the
@@ -112,6 +216,7 @@ class Exif(BaseModel):
     """
 
     filter_tags: Dict
+    lowercase_keys: bool = True
 
     filter_config: ClassVar[FilterConfig] = FilterConfig(
         name="exif",
@@ -119,23 +224,41 @@ class Exif(BaseModel):
         dirs=False,
     )
 
-    def __init__(self, *args, filter_tags: Optional[Dict] = None, **kwargs):
+    def __init__(
+        self,
+        *args,
+        filter_tags: Optional[Dict] = None,
+        lowercase_keys: bool = True,
+        **kwargs,
+    ):
         # exif filter is used differently from other filters. The **kwargs are not
         # filter parameters but all belong into the filter_tags dictionary to filter
         # for specific exif tags.
-        # *args are tags filtered without a value, like ["gps", "image.model"].
         params = filter_tags or dict()
         params.update(kwargs)
+        # *args are tags filtered without a value, like ["gps", "image.model"].
         for arg in args:
             params[arg] = None
-        super().__init__(filter_tags=params)
+        super().__init__(filter_tags=params, lowercase_keys=lowercase_keys)
 
     def pipeline(self, res: Resource, output: Output) -> bool:
         assert res.path is not None, "Does not support standalone mode"
-        data = read_exif_data(res.path)
-        parsed = parse_and_categorize(data)
+
+        # gather the exif data in a dict
+        if exiftool_available():
+            data = exiftool_read(path=res.path)
+        else:
+            data = exifread_read(path=res.path)
+
+        # lowercase keys if wanted
+        if self.lowercase_keys:
+            data = lowercase_keys_recursive(data)
+
+        # convert strings to datetime objects where possible
+        parsed = convert_recursive(data)
+
         res.vars[self.filter_config.name] = parsed
-        return matches_tags(self.filter_tags, parsed)
+        return matches_tags(self.filter_tags, data)
 
 
 if __name__ == "__main__":
@@ -143,7 +266,7 @@ if __name__ == "__main__":
 
     # Usage:
     # python organize/filters/exif.py tests/resources/images-with-exif/3.jpg
-    data = read_exif_data(Path(sys.argv[1]))
-    parsed = parse_and_categorize(data)
+    data = exifread_read(Path(sys.argv[1]))
     print(data)
-    print(parsed)
+    data = exiftool_read(Path(sys.argv[1]))
+    print(data)
