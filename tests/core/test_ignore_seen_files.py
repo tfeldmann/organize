64b9667eb315c33305e12dc5a6fd7290fe979b29
Thomas Feldmann <mail@tfeldmann.de>
2024-01-07 19:47:38 +0100

    Add skip_path test (fixes #200)

--- tests/core/test_ignore_seen_files.py
+++ tests/core/test_ignore_seen_files.py
@@ -27,3 +27,25 @@ def test_ignore_seen_files(fs):
             "bar.txt": "",
         }
     }
+
+
+def test_issue_200(fs):
+    # https://github.com/tfeldmann/organize/issues/200
+    config = """
+    # try to extract the first date from the file and rename it accordingly
+    rules:
+      - name: date_rename
+        locations: scan
+        filters:
+          - name
+          - extension: txt
+          - filecontent: '(?P<m>[0123]\d)\.(?P<d>[01]\d)\.(?P<y>[12][09]\d\d)'
+        actions:
+          - rename: "{filecontent.y}-{filecontent.m}-{filecontent.d}_{name}.txt"
+    """
+    make_files({"20220401_173738.txt": "Datum: 23.03.2022"}, "scan")
+    Config.from_string(config).execute(simulate=False)
+    result = read_files("/scan")
+    assert result == {
+        "2022-23-03_20220401_173738.txt": "Datum: 23.03.2022",
+    }
