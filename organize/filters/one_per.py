91272f0b9d88fda143adb9e32d5ee873cf509a28
Kel Byers <ruferto@gmail.com>
2026-06-28 23:21:57 +0000

    OnePer filter to find multiple files within a period (hour, day, etc.)
    - lastmodified timestamp comparisons
    - creation timestamp comparisions
    - reverse ordering for timestamp comparisons
    - add to `res.vars`

--- organize/filters/one_per.py
+++ organize/filters/one_per.py
@@ -1,8 +1,8 @@
-import os
 from pathlib import Path
-from typing import ClassVar, Literal
+from typing import ClassVar, Literal, Tuple
 
-import arrow
+from arrow import Arrow
+from arrow import get as arrow_get
 from pydantic.config import ConfigDict
 from pydantic.dataclasses import dataclass
 
@@ -10,12 +10,27 @@ from organize.filter import FilterConfig
 from organize.output import Output
 from organize.resource import Resource
 
+# allowable values for `period`
 Period = Literal[
     "month",
     "day",
     "hour",
     "minute",
     "second",
+]
+
+# allowable values for `detect_the_one_by`
+DetectionMethod = Literal[
+    "first_seen",
+    "-first_seen",
+    "last_seen",
+    "-last_seen",
+    "name",
+    "-name",
+    "created",
+    "-created",
+    "lastmodified",
+    "-lastmodified",
 ]
 
 
@@ -27,7 +42,7 @@ class OnePer:
     for the earliest file from the time period.
 
     Attributes:
-        detect_original_by (str):
+        detect_the_one_by (str):
             Detection method to distinguish between original and duplicate.
             Possible values are:
 
@@ -46,35 +61,160 @@ class OnePer:
             - `"minute"`
             - `"hour"`
             - `"day"`
-            - `""`
+            - `"month"`
     """
 
     period: Period = "hour"
-    detect_the_one_by: str = "modified"
+    detect_the_one_by: DetectionMethod = "lastmodified"
 
     filter_config: ClassVar[FilterConfig] = FilterConfig(
         name="one_per", files=True, dirs=True
     )
 
     def __post_init__(self):
-        self._seen_files: set["os.PathLike"] = set()
-        self._the_one_for_period: dict["arrow.Arrow", "os.PathLike"] = dict()
+        """set up initial state of the filter, based on the filter options"""
+        self._detect_the_one_by = self.detect_the_one_by
+        self._detect_the_one_reverse = False
+        if self.detect_the_one_by.startswith("-"):
+            self._detect_the_one_by = self.detect_the_one_by[1:]
+            self._detect_the_one_reverse = True
 
-    def get_period(self, file: Path) -> arrow.Arrow:
-        ts = arrow.get(file.stat().st_mtime)
-        return ts.floor(self.period)
+        # track files we have already seen before
+        self._seen_files: set[Path] = set()
+        # for each period, the file that is "the_one"
+        self._the_one_for_period: dict[Arrow, Path] = dict()
+        # does our detection method need the timestamp?
+        self._track_timestamps: bool = False
+        if self._detect_the_one_by in ["created", "lastmodified"]:
+            # this detection method uses the file timestamp, so keep it for
+            # each period
+            self._ts_for_the_one: dict[Arrow, Arrow] = dict()
+            self._track_timestamps = True
+
+    def get_period(self, file: Path) -> Tuple[Arrow, Arrow]:
+        """get the timestamp for the file and the period for the timestamp
+
+        A period is the earliest possible timestamp for grouping files that are
+        in the same group, regardless of whether we are looking for the oldest
+        or newest files.
+
+        Since we always have to get the timestamp in order to get the period, we
+        return it here so that we don't have to get it again if we need to use
+        it.
+
+        :param file: the path to the file whose period we want
+        :type file: Path
+
+        :return: a tuple with (period, timestamp)
+        :rtype: tuple
+        """
+        if self._detect_the_one_by == "created":
+            # if detection method is file creation time, use that as timestamp
+            ts = arrow_get(file.stat().st_ctime)
+        else:
+            # for all other detection methods, use the file modification time
+            ts = arrow_get(file.stat().st_mtime)
+        # period for `path`
+        period = ts.floor(self.period)
+
+        return period, ts
+
+    def is_the_one(self, period: Arrow, path: Path, ts: Arrow) -> bool:
+        """check if `path` should be the one
+
+        :param period: - the period for `path`
+        :type period: Arrow
+        :param path: - the file we are checking
+        :type path: Path
+        :param ts: - the timestamp for `path`
+        :type ts: Arrow
+
+        :return: is `path` the new `the_one`
+        :rtype: bool
+        """
+        it_is = ts < self._ts_for_the_one[period]
+        if self._detect_the_one_reverse:
+            return not it_is
+        return it_is
+
+    def update_the_one(self, period: Arrow, path: Path, ts: Arrow) -> None:
+        """update our internal tracker for `the_one`
+
+        :param period: - the period for `path`
+        :type period: Arrow
+        :param path: - the Path to `the_one`
+        :type path: Path
+        :param ts: - the timestamp for `path`
+        :type ts: Arrow
+        """
+        self._the_one_for_period[period] = path
+        if self._track_timestamps:
+            self._ts_for_the_one[period] = ts
 
     def pipeline(self, res: Resource, output: Output) -> bool:
+        """Process one file through this filter.
+
+        Since we are looking for files that can be grouped together by their
+        timestamps, this may emit:
+        - `False`, stop processing this file in other filters or actions
+        - `True`, process this file in other filters/actions
+        - `True`, process a different file in other filters/actions
+
+        The actual file that gets processed further down the pipeline is
+        identified by this method returning `True`, and by the value of
+        `res.path`. We can change which file other pipelines will process by
+        updating `res`.
+
+        :param res: the Resource for this filter to process; this might be
+                    modified to force futher pipelines to use a different
+                    Resource
+        :type res: Resource
+        :param output: not used in this filter
+        :type output: Output
+        :return: continue processing `res` in other filters/actions
+        :rtype: bool
+        """
         assert res.path is not None, "Does not support standalone mode"
 
+        # skip if symlink
         if res.path.is_symlink():
             return False
 
+        # skip if we have already processed this file
         if res.path in self._seen_files:
             return False
         self._seen_files.add(res.path)
 
-        period = self.get_period(res.path)
-        self._the_one_for_period[period] = res.path
+        # get the period for this file
+        period, ts = self.get_period(res.path)
 
-        return False
+        # get the_one, if this is the first file in period, it is the_one
+        the_one = self._the_one_for_period.get(period, res.path)
+
+        # assume we do not want to continue processing this file
+        process_it: bool = False
+
+        if the_one != res.path:
+            # if the_one is different from current file, at least one of them
+            # will be processed (can only be same for first file)
+            process_it = True
+
+            # compare against current one for this period
+            if self.is_the_one(period, res.path, ts):
+                # this file is the new the_one
+                previous = the_one
+                new_one = res.path
+
+                # save the_one and its timestamp
+                self.update_the_one(period, res.path, ts)
+
+                # switch which file gets processed
+                res.path = previous
+                the_one = new_one
+        else:
+            # no files in this period yet, this one is the one
+            self.update_the_one(period, the_one, ts)
+
+        # done processing, update vars and return value
+        res.vars[self.filter_config.name] = {"the_one": the_one}
+        return process_it
--- tests/filters/test_oneper.py
+++ tests/filters/test_oneper.py
@@ -1,12 +1,13 @@
 import os
 from pathlib import Path
+from typing import Generator, List, Optional, Tuple, Union
 
 import pytest
 from arrow import Arrow
 from pydantic_core import ValidationError
 
 from organize.filters import OnePer
-from organize.filters.one_per import Period
+from organize.filters.one_per import DetectionMethod, Period
 from organize.output import Default as Output
 from organize.resource import Resource
 
@@ -17,7 +18,10 @@ def period(request) -> Period:
     return request.param
 
 
-def make_a_path(file: Path | str, timestamp: Arrow | None = None) -> Path:
+def make_a_path(
+    file: str,
+    timestamp: Optional[Arrow] = None,
+) -> Path:
     """make sure file exists and has a specific timestamp"""
     # make sure file exists
     path = Path(file)
@@ -79,7 +83,7 @@ def test_valid_periods(period):
 def test_invalid_period():
     """an invalid period should raise an exception"""
     try:
-        OnePer(period="fortnight")
+        OnePer(period="fortnight")  # type: ignore[assignment]
         assert False, "Unexpected period allowed"
     except ValidationError:
         assert True
@@ -108,3 +112,104 @@ def test_tracks_file_period(fs, period):
 
     # with only one file, it should be the one for its period
     assert op._the_one_for_period[file_period] is f
+
+
+@pytest.fixture
+def files_with_relative_ts(
+    fs, offsets: List[int], order: Optional[List[int]]
+) -> Generator[List[Path], None, None]:
+    """fixture to create several files with timestamps
+
+    Parameters:
+    - `offsets`: an array of offsets in seconds for the modified timestamp;
+      the value of the offset is added to the time when the function is called
+    - `order`: (optional) the order to create the files in; by default, they
+      will be created in the natural order provided by the offsets; the value of
+      order will be used as the index into the `offsets` list
+
+    Example explaining `offsets` and `order`:
+    - `offsets = [2, 0, 1]`
+    - `order = None`:
+    - Three files will be created:
+      - index 0: `/file_0` will be created with its mtime with 2 seconds
+        (from `offsets[0] = 2`)
+      - index 1: `/file_1`, mtime is +0 (`offsets[1] = 0`)
+      - index 2: `/file_2`, mtime is +1 (`offsets[2] = 1`)
+
+    Example 2:
+    - `offsets = [5, 5, 5]`
+    - `order = [2, 0, 1]`
+    - Three files will be created, in the following order:
+      - order[0] -> 2 -> offsets[2]: `/file_2`, mtime is +5
+      - order[1] -> 0 -> offsets[0]: `/file_0`, mtime is +5
+      - order[2] -> 1 -> offsets[1]: `/file_1`, mtime is +5
+    """
+    now = Arrow.now()
+
+    paths: List[Path] = list()
+    for i in range(len(offsets)):
+        idx = order[i] if order else i
+        offset = offsets[idx]
+        mtime = now.shift(seconds=offset)
+        paths.append(make_a_path(f"/file_{idx}", mtime))
+    yield sorted(paths)
+
+
+@pytest.fixture
+def method_and_expect(
+    files_with_relative_ts, method: DetectionMethod, expected: int
+) -> Generator[Tuple[DetectionMethod, List[Path], Path], None, None]:
+    """"""
+    paths = files_with_relative_ts
+    yield method, paths, (paths[expected])
+
+
+@pytest.mark.parametrize(
+    ["method", "expected", "offsets", "order", "acted"],
+    [
+        ("lastmodified", 1, [2, 0, 1], None, [False, 0, 2]),
+        ("lastmodified", 2, [1, 2, 0], None, [False, 1, 0]),
+        ("-lastmodified", 1, [0, 2, 1], None, [False, 0, 2]),
+        ("-lastmodified", 2, [0, 1, 2], None, [False, 0, 1]),
+        ("created", 1, [5, 5, 5], [1, 0, 2], [False, 0, 2]),
+        ("-created", 1, [5, 5, 5], [0, 2, 1], [False, 0, 2]),
+    ],
+)
+def test_selects_one_with_method(method_and_expect, acted: List[Union[bool, int]]):
+    ## arrange
+    method, paths, expected = method_and_expect
+    op = OnePer(detect_the_one_by=method)
+
+    ## act
+    a: List[bool] = list()
+    r: List[Resource] = list()
+    for path in paths:
+        res = Resource(path)
+        act = op.pipeline(res, Output())
+        a.append(act)
+        r.append(res)
+
+    ## assert
+    expected_one = paths[0]
+    for i, act in enumerate(a):
+        res = r[i]
+        expected_act = acted[i]
+        expected_a = not isinstance(expected_act, bool)
+
+        # pipeline call return expected True|False
+        assert act is expected_a
+
+        # the value of res.path is correct
+        expected_res_path: Path
+        if not expected_a:
+            # value of res.path should equal the path the pipeline processed
+            expected_res_path = paths[i]
+        else:
+            # value of res.path should be updated with the previous `the_one`
+            expected_res_path = paths[expected_act]
+        assert expected_res_path == res.path
+
+        # the vars for our filter should be updated
+        if expected_act != i:
+            expected_one = paths[i]
+        assert expected_one == res.vars[op.filter_config.name]["the_one"]
