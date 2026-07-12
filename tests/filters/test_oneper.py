d3e05bea371d1c0894c079a7b8678621f80a2a66
Kel Byers <ruferto@gmail.com>
2026-07-12 18:40:32 +0000

    still having timing issues (maybe)

--- tests/filters/test_oneper.py
+++ tests/filters/test_oneper.py
@@ -169,7 +169,7 @@ def files_with_relative_ts(
         idx = order[i] if order else i
         if i > 0:
             # force a delay between ctime stamps
-            sleep(0.001)
+            sleep(0.01)
         offset = offsets[idx]
         mtime = now.shift(seconds=offset)
         paths.append(make_a_path(f"/file_{idx}", mtime))
